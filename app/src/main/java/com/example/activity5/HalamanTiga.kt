a19c216d6b1deceb736a5d79e6d14695972252e4
febyan <febyan.putra.ft21@mail.umy.ac.id>
2023-11-16 14:48:53 +0700

    membuat halaman tiga

--- app/src/main/java/com/example/activity5/HalamanTiga.kt
+++ app/src/main/java/com/example/activity5/HalamanTiga.kt
@@ -1,2 +1,8 @@
 package com.example.activity5
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanTiga(){}
