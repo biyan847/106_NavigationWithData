d19dbd4ea10b0438ca809dd5e8f1cecc0e6e9265
febyan <febyan.putra.ft21@mail.umy.ac.id>
2023-11-16 13:59:50 +0700

    membuat halaman tiga

--- app/src/main/java/com/example/activity5/data/OrderUistate.kt
+++ app/src/main/java/com/example/activity5/data/OrderUistate.kt
@@ -1,7 +1,10 @@
 package com.example.activity5.data
 
-data class OrderUistate(
-    val jumlah: Int = 0,
+data class OrderUistate (
+    val jumlah : Int = 0,
     val rasa : String = "",
     val harga : String = "",
+    val nama : String ="",
+    val noTelp : String = "",
+    val alamat : String = ""
 )
